aae8319946b29bf5746abe446418090c014b0822
Indrakant Dana <ik.dana24@gmail.com>
2022-04-09 00:17:59 +0530

    Add .env files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -22,4 +22,4 @@ export const API_URLS = {
   searchUsers: (searchText) => `${API_ROOT}/users/search?text=${searchText}`,
 };
 
-export const LOCALSTORAGE_TOKEN_KEY = '__codeial_token__'; //using this key, we will store the token when user logs in
+export const LOCALSTORAGE_TOKEN_KEY = process.env.LOCALSTORAGE_TOKEN_KEY; //using this key, we will store the token when user logs in
